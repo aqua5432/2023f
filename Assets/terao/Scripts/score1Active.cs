86c04884101aacfee9e7d28ed50a279db5a47a2f
terao05 <141928821+terao05@users.noreply.github.com>
2023-09-14 00:56:19 +0900

    実績
    ほぼUIのみ

---
+++ Assets/terao/Scripts/score1Active.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score1Active : MonoBehaviour
+{
+    void Update ()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/terao/Scripts/score2Active.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score2Active : MonoBehaviour
+{
+    void Update()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
